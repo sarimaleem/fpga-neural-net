a6b345921af0af58e00b56aa3f62fdab5ed16dcc
Sarim Aleem <sarimaleem99@gmail.com>
2024-04-25 16:52:02 -0500

    basic message handling done

--- neuralnet.sv
+++ neuralnet.sv
@@ -4,32 +4,31 @@ module neuralnet (
     input wire rst_n, // Active low reset
     input wire write_enable, // Enable writing to memory
     input wire [1:0] buttons, // Button inputs to select memory
-    output wire [5:0] LED // LED output
+    output logic [5:0] LED // LED output
 );
     // Memory to store bytes from GPIO
-    reg [7:0] bytes[0:3];
-    reg [1:0] count = 2'b00; // 4 possible places you can write, initialized to 0
+    logic [7:0] bytes [3:0];
+    logic [3:0] count; // 4 possible places you can write, initialized to 0
 
-    always @(posedge pi_clk or negedge rst_n) begin
-        if (!rst_n) begin
+    initial begin
+        count = 0;
+    end
+
+    assign LED = bytes[buttons][5:0];
+
+    always @(posedge pi_clk or posedge rst_n) begin
+        if (rst_n) begin
             // Reset count and bytes
             count <= 2'b00;
             bytes[0] <= 8'd0;
             bytes[1] <= 8'd0;
             bytes[2] <= 8'd0;
             bytes[3] <= 8'd0;
-        end else if (write_enable) begin
-            if (count < 3) begin
-                bytes[count] <= gpio_pin;
-                count <= count + 1;
-            end else if (count == 3) begin
-                bytes[count] <= gpio_pin;
-                // Optionally reset or stop incrementing count to avoid overflow
-                count <= 0; // Wrap around or handle as required
-            end
+        end 
+        else if (write_enable && count < 4) begin
+            bytes[count] <= gpio_pin;
+            count <= count + 1;
         end
     end
 
-    // Connect the selected byte's lower 6 bits to the LEDs
-    assign LED = bytes[buttons][5:0];
 endmodule
