d3bbb0c3864b57479de8c30350a7667cc5bb16de
Sarim Aleem <sarimaleem99@gmail.com>
2024-04-28 23:32:23 -0500

    stuff

--- neuralnet.sv
+++ neuralnet.sv
@@ -7,32 +7,59 @@ module neuralnet (
     output logic [5:0] LED // LED output
 );
 
-    parameter int LENGTH = 28;
-    parameter int WIDTH = 28;
+    parameter int HEIGHT = 20;
+    parameter int WIDTH = 30;
+    parameter int DEPTH = 3; // Depth of 3 for RGB color channels
 
-    logic [7:0] bytes [LENGTH*WIDTH - 1:0];
-    signed logic [7:0] weights [50:0];
+    logic [7:0] image [HEIGHT * WIDTH * DEPTH]; // 3D array to store image data
     logic [31:0] count;
+
+    logic [31:0] h;
+    logic [31:0] w;
+    logic [31:0] d;
 
     initial begin
         count = 0;
+        h = 0;
+        w = 0;
+        d = 0;
     end
-
 
     always @(posedge pi_clk or posedge rst) begin
         if (rst) begin
-            // Reset the count and byte storage
+            // Reset the count and clear the image storage
             count = 0;
-            for (int i = 0; i < LENGTH*WIDTH; i++) begin
-                bytes[i] = 8'd0;
+            h = 0;
+            w = 0;
+            d = 0;
+            for (int i = 0; i < HEIGHT * WIDTH * DEPTH; i++) begin
+                image[i] = 8'd2;
             end
+            // for (int height = 0; height < HEIGHT; height++) begin
+            //     for (int width = 0; width < WIDTH; width++) begin
+            //         for (int depth = 0; depth < DEPTH; depth++) begin
+            //             image[height][width][depth] = 8'd0;
+            //         end
+            //     end
+            // end
         end 
-        else if (write_enable && count < LENGTH*WIDTH) begin
-            bytes[count] = gpio_pin;
+        else if (write_enable && count < HEIGHT*WIDTH*DEPTH) begin
+            // Calculate indices based on the count
+            d = d + 1;
+            if (d == DEPTH) begin
+                d = 0;
+                w = w + 1;
+            end
+            if (w == WIDTH) begin
+                w = 0;
+                h = h + 1;
+            end
+            image[count] = gpio_pin;
             count = count + 1;
         end
     end
 
-    // assign LED = 5'b10101;
-    assign LED = bytes[155][5:0];
+    assign LED = image[10][5:0];
+    // assign LED = 6'b101010;
+
 endmodule
--- rps-classifier/Image_preprocess.py
+++ rps-classifier/Image_preprocess.py
@@ -22,26 +22,6 @@ def main():
 
         hand_mask = cv2.inRange(img, lower_green, )
 
-        # for i in range(LENGTH):
-        #     for j in range(WIDTH):
-        #         if img_array[i][j][0] >= LOWER_GREEN_ONE and img_array[i][j][0] <= UPPER_GREEN_ONE and \
-        #                 img_array[i][j][1] >= LOWER_GREEN_TWO and img_array[i][j][1] <= UPPER_GREEN_TWO and \
-        #                 img_array[i][j][2] >= LOWER_GREEN_THREE and img_array[i][j][2] <= UPPER_GREEN_THREE:
-        #             print("0", end="")
-        #         else: print("1", end="")
-        #     print()
-
-        # with open("dummy/data_init_dummy.sv", "w") as output:
-        #     output.write("module data_init (inout logic [7:0] image [{LENGTH-1}:0][{WIDTH-1}:0]);\n\n")
-        #     output.write(f"logic [7:0] image [{LENGTH-1}:0][{WIDTH-1}:0];\n\n")
-        #     output.write("initial begin\n")
-        #     for i in range(LENGTH):
-        #         for j in range(WIDTH):
-        #             for k in range(3):
-        #                 element = img_array[i][j][k]
-        #                 output.write(f"    image[{i}][{j}][{k}] = 8'd{element};\n")
-        #     output.write("end\n")
-        #     output.write("endmodule\n")
 
 if __name__ == "__main__":
     main()
